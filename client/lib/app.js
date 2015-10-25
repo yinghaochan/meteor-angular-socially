180a0f46811f4423ed18f170e8f76dfeaa846615
Dotan Simha <dotansimha@gmail.com>
2015-10-25 21:17:54 +0200

    Step 21.11: Add module isolation for browser

--- client/lib/app.js
+++ client/lib/app.js
@@ -14,6 +14,9 @@ var modulesToLoad = [
 if (Meteor.isCordova) {
   modulesToLoad = modulesToLoad.concat(['socially.mobile']);
 }
+else {
+  modulesToLoad = modulesToLoad.concat(['socially.browser']);
+}
 
 angular.module('socially', modulesToLoad);
 
