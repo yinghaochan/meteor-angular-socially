0fe97c5f191a4da7b5c45232e9a1d7c2c45d814e
Dotan Simha <dotansimha@gmail.com>
2015-10-31 20:43:05 +0200

    Step 22.17: Use PartiesListCtrl with the view

--- packages/socially-mobile/client/routes.js
+++ packages/socially-mobile/client/routes.js
@@ -18,7 +18,8 @@ angular.module('socially.mobile').config(function($stateProvider) {
       url: '/parties',
       views: {
         main: {
-          templateUrl: '/packages/socially-mobile/client/parties/views/parties-list.html'
+          templateUrl: '/packages/socially-mobile/client/parties/views/parties-list.html',
+          controller: 'PartiesListCtrl'
         }
       }
     });
